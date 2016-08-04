90996a333f6678c6ecde8a9de1adf3f3b4c71c96
reactexcel <ravi.etech2015@gmail.com>
2016-08-04 19:07:47 +0530

    send mail method (#1)
    send mail method

---
+++ src/app/config/index.jsx
@@ -0,0 +1,6 @@
+export const config_ENV = {
+  "email": {
+    "_id": "excellenceseo@gmail.com",
+    "_password": "java@123"
+  }
+}
--- src/app/server/index.js
+++ src/app/server/index.js
@@ -1,4 +1,6 @@
-//Import Collections and methods
+import '../methods/sendMail.js';
+import {config_ENV} from 'src/app/config';
 
-import './publications';
-import 'TodoApp/methods';
+Meteor.startup(function () {
+  process.env.MAIL_URL = 'smtp://'+config_ENV.email._id+':'+config_ENV.email._Password+'@smtp.gmail.com:25';
+});
