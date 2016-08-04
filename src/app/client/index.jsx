5b2cc0524e811fe227e4cae9cab7eef8e5020236
reactexcel <ravi.etech2015@gmail.com>
2016-08-04 13:31:08 +0530

    File structure

---
+++ src/app/client/index.jsx
@@ -0,0 +1,35 @@
+import thunk from 'redux-thunk';
+import ReactDOM from 'react-dom';
+import Immutable from 'immutable';
+import {Meteor} from 'meteor/meteor';
+import { Provider } from 'react-redux';
+import createLogger from 'redux-logger';
+import {reducer} from './reducers/index.jsx';
+import invariant from 'redux-immutable-state-invariant';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { createStore, applyMiddleware, compose } from 'redux'
+import {Router , Route, IndexRoute, hashHistory} from 'react-router'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+export let isMonitorAction;
+
+const logger = createLogger();
+injectTapEventPlugin();
+
+Meteor.startup(
+  () => {
+    let store = createStore(reducer,Immutable.Map({}),compose(
+      applyMiddleware(invariant(), logger,thunk),
+      window.devToolsExtension ? window.devToolsExtension({
+        getMonitor: (monitor) => { isMonitorAction = monitor.isMonitorAction; }
+        }) : f => f
+      ));
+
+    ReactDOM.render(
+      <MuiThemeProvider>
+        <Provider store={store}>
+          
+        </Provider>
+      </MuiThemeProvider>, document.getElementById('app-container'));
+  }
+);
