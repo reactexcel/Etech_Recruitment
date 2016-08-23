6a5f6060eead383d5a1daad0071a7f09888ca633
reactexcel <ravi.etech2015@gmail.com>
2016-08-23 12:58:51 +0530

    Progress bar added

--- src/app/client/components/sendmail/index.jsx
+++ src/app/client/components/sendmail/index.jsx
@@ -8,54 +8,20 @@ import TextField from 'material-ui/TextField';
 
 import {Menu, MenuItem} from 'material-ui/Menu'
 import FlatButton from 'material-ui/FlatButton'
-import RaisedButton from 'material-ui/RaisedButton';
 import Avatar from 'material-ui/Avatar';
 import _ from 'lodash'
 import verge from 'verge';
-//import EditableDiv from 'react-wysiwyg-editor';
-import FroalaEditor from 'react-froala-editor';
-//import ReactQuill from 'react-quill';
-//var ReactQuill = require('react-quill');
 
 class SendEmail extends React.Component {
     constructor( props ){
         super( props );
-        this.state={
-            to:'',
-            subject:'',
-            content:'test',
-            value:''
-        }
-        this.editor=''
-        this.handleContentChange  = this.handleContentChange.bind( this )
-    }
-    componentWillMount(){
-
     }
     componentDidMount(){
-
-    }
-    handleContentChange(e) {
-        console.log('*******To******')
-        console.log(this.refs.to.input.value)
-        console.log('*******subject******')
-        console.log(this.refs.subject.input.value)
-        console.log('*******mail******')
-        console.log(this.refs.mail.props.value)
     }
     componentWillReceiveProps( props ){
-        console.log(this.refs.mail)
     }
-    getHtml(val){
 
-    }
     render(){
-     let  editorStyle = {
-        overflow: 'auto',
-        width: '100%',
-        height: 400,
-        maxHeight: 400
-    }
         let count_unread_emails = ""
         if( typeof this.props.inbox.count_unread_emails != 'undefined' && this.props.inbox.count_unread_emails > 0 ){
             count_unread_emails  = "(" + this.props.inbox.count_unread_emails + ")"
@@ -101,48 +67,19 @@ class SendEmail extends React.Component {
             <div className='row' style={{background: '#fff'}}>
                 <div className="col-xs-12" style={{background: 'antiquewhite',padding: '10px',borderBottom: '1px solid gainsboro'}}>
                     <div className="col-xs-12">
-                       <b><i>Ceeate New</i></b> <br />
+                       <b><i>Send new mail</i></b> <br />
                     </div>
                 </div>
                 <div className="col-xs-12" style={{fontSize: '20px',padding: "10px 20px 20px",borderBottom: '1px solid gainsboro'}}>
                     <TextField
-                    type='email'
-                    ref='to'
                     floatingLabelText="To"
-                    fullWidth={true}
-                    floatingLabelFixed={true}
+                    fullWidth='true'
                     />
-                    <TextField
-                    ref='subject'
-                    floatingLabelText="Subject"
-                    fullWidth={true}
-                    floatingLabelFixed={true}
-                    />
-                    <br />
-
                 </div>
                 <div className="col-xs-12" style={{marginBottom: '15px',borderBottom: '1px solid gainsboro'}}>
-                   <FroalaEditor
-                   base='https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.3.4'
-                    style={editorStyle}
-                    contentChanged={(e,editor)=>{
-                        this.setState({value:editor.value})
-                        console.log(this.refs.mail.props.value)
-                    }}
-                    ref='mail'
-                    value={this.state.value}
-                    fileP={true}
-                    toolbarBottom={true}
-                    config={{toolbarBottom: true}}
-                   />
 
                 </div>
-                 <RaisedButton
-                    style={{float:'right',margin:'20px'}}
-                    label='save'
-                    onClick={this.handleContentChange}
-                    primary={true}
-                    ></RaisedButton>
+
             </div>
         </div>
         </div>
