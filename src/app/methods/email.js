6e5b03a37d4b9e628e36aa90bcec49a50018becb
reactexcel <ravi.etech2015@gmail.com>
2017-01-17 13:17:49 +0530

    attchment completed

--- src/app/methods/email.js
+++ src/app/methods/email.js
@@ -114,60 +114,6 @@ Meteor.methods({
       });
     }
 
-    // if( typeof emailData[0].attachments !== 'undefined'){
-    //   if(typeof emailData[0].attachments[0].link == 'undefined'){
-    //     SyncedCron.add({
-    //       name: email_id,
-    //       emailData: emailData[0],
-    //       user_id: user_id,
-    //       BASE_URL: config_ENV._BASE_URL,
-    //       PARAMS: function(){
-    //           let imapEmails = this.server();
-    //           _.forEach(imapEmails, (e) => {
-    //             if(typeof e.smtp == 'undefined'){
-    //               this.imapEmail = e;
-    //             }
-    //           });
-    //             return  "email="+this.imapEmail.emailId+
-    //                     "&pass="+this.imapEmail.password+
-    //                     "&date="+ moment(this.emailData.email_timestamp).format('YYYY-MM-DD')+
-    //                     "&host="+this.imapEmail.server+
-    //                     "&port="+this.imapEmail.port+
-    //                     "&encryp="+this.imapEmail.encrypt+
-    //                     "&email_id="+this.emailData.email_id;
-    //                   },
-    //       API_URL: function(){ return this.BASE_URL + this.PARAMS();},
-    //       server: function(){return Config.find({emailId: this.emailData.m_source_email_id}).fetch()},
-    //       imapEmail: {},
-    //       schedule: function(parser) {
-    //         return parser.text('every 2 secs');
-    //       },
-    //       job: function() {
-    //         console.log('api called------------');
-    //         try{
-    //           let response = HTTP.call("GET", this.API_URL() );
-    //           let responseData = response.content
-    //           let jsonData = [];
-    //           if( typeof responseData != 'undefined' ){
-    //             jsonData = JSON.parse( responseData );
-    //           }
-    //           if( jsonData.data.length > 0 ){
-    //             EmailsStore.update({_id: this.name},{$set:{ attachments: jsonData.data[0].attachments}});
-    //             console.log(EmailsStore.find({_id: this.name}).fetch()[0].attachments);
-    //             Meteor.ClientCall.apply(this.user_id, 'updateAttachment', [jsonData.data], function(err ){
-    //               console.log('Done!');
-    //             });
-    //             SyncedCron.remove(this.name);
-    //           }
-    //         }catch(err){
-    //           console.log("ERR in method firstEmail -->> ",err);
-    //         }
-    //       }
-    //     });
-    //   }else{
-    //     console.log('attachment Link is already available');
-    //   }
-    // }
     return emailData
   },
   getAttchementUpdatedMail : function(email_id, user_id){
